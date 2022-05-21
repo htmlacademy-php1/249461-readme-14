6d1a32443d485cdcd409bc4a595715b4a0d2cdfc
skravtsov <s.kravtsov@hotmail.com>
2022-05-21 15:03:16 +0300

    Обмен сообщениями часть 2

--- messages.php
+++ messages.php
@@ -5,11 +5,26 @@ require_once 'functions.php';
 require_once 'db_connect.php';
 require_once 'session.php';
 
-$title = 'Личные сообщения';
-$active_page = 'messages';
 
-// Адрес чата на который идет редирект после успешной отправки сообщения
-$current_url = $_SERVER['HTTP_REFERER'];
+/**
+ * Фукнция отрисовки страницы обмена сообщениями
+ * @param array $content_data Массив с параметрами контента страницы
+ * @param array $current_user Массив с данными текущего пользователя
+ * @return void Контент страницы
+ */
+function print_messages_page(array $content_data, array $current_user)
+{
+    $content = include_template('messages.php', $content_data);
+
+    $layout_content = include_template('layout.php', [
+        'content' => $content,
+        'title' => 'Личные сообщения',
+        'current_user' => $current_user,
+        'active_page' => 'messages'
+    ]);
+
+    print($layout_content);
+}
 
 /**
  * Получает последнее сообщение в чате между указаными пользователями
@@ -18,16 +33,18 @@ $current_url = $_SERVER['HTTP_REFERER'];
  * @param int $user2 Активный пользователь
  * @return mixed Ошибка БД, или массив с данными последнего сообщения в переписке
  */
-function get_last_message(mysqli $db_connect, int $user1, int $user2) {
+function get_last_message(mysqli $db_connect, int $user1, int $user2)
+{
     $sql = "SELECT dt_add, message, sender FROM messages WHERE (sender = ? AND receiver = ?) OR (receiver = ? AND sender = ?) ORDER BY dt_add DESC";
+    if (empty(get_db_data($db_connect, $sql, [$user1, $user2, $user1, $user2]))) {
+        return '';
+    }
     return get_db_data($db_connect, $sql, [$user1, $user2, $user1, $user2])[0];
 }
 
-// Получение всех пользователей из таблицы с сообщениями
 $sql_chats = "SELECT sender,receiver FROM messages WHERE sender = ? OR receiver = ? ORDER BY dt_add DESC";
 $user_chats = get_db_data($db_connect, $sql_chats, [$current_user['id'], $current_user['id']]);
 
-// Формарование массива со списком id пользователей с которыми велась переписка ранее; id могут повторятся
 $users_id = [];
 foreach ($user_chats as $user_chat) {
     if ($user_chat['sender'] != $current_user['id']) {
@@ -38,87 +55,111 @@ foreach ($user_chats as $user_chat) {
     }
 }
 
-// Убираем дублированные id без сохранения ключей исходного массива
 $users_id = array_values(array_unique($users_id));
 
-// id активного чата = id пользователя с которым велась переписка; Или 0 если у пользователя нет чатов
 $active_chat = 0;
 
-// Если есть гет параметр с ID чата или список пользователей с перепиской не пустой получаем id активного чата
 if (isset($_GET['chat']) || !empty($users_id)) {
     $active_chat = $_GET['chat'] ?? $users_id[0];
 }
+if ($active_chat === 0) {
+    $content_data = [
+        'no_chats' => true,
+        'current_user' => $current_user
+    ];
 
-// При открытии чата отмечаем все непрочитанные сообщения прочитанными для этого чата
+    print_messages_page($content_data, $current_user);
+    die();
+}
+
+if ($active_chat !== 0 && !in_array($active_chat, $users_id)) {
+    array_unshift($users_id, $active_chat);
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $sql = "UPDATE messages SET is_read = 1 WHERE sender = ? AND receiver = ?";
     $stmt = db_get_prepare_stmt($db_connect, $sql, [$active_chat, $current_user['id']]);
     mysqli_stmt_execute($stmt);
 }
 
-// Обновляем кол-во непрочитанных сообщений user menu в шапке сайта
-$current_user['new_messages'] = count_not_read_messages($db_connect,$current_user['id']);
+$current_user['new_messages'] = count_not_read_messages($db_connect, $current_user['id']);
 
-// Спиок чатов которые велись ранее с данными пользователей, последним сообщением, и кол-вом непрочитанных, если такие есть
 $sql_users = "SELECT id, login, avatar FROM users WHERE id = ?";
 $users = [];
 foreach ($users_id as $key => $user_id) {
-    $users[$key] = get_db_data($db_connect,$sql_users,[$user_id])[0];
+    if (empty(get_db_data($db_connect, $sql_users, [$user_id]))) {
+        header('HTTP/1.1 404 Not Found', true, 404);
+        print "Пользователя с таким id не найдено";
+        die();
+    }
+
+    $users[$key] = get_db_data($db_connect, $sql_users, [$user_id])[0];
     $users[$key]['message'] = get_last_message($db_connect, $user_id, $current_user['id']);
-    $users[$key]['not_read'] = count_not_read_messages($db_connect,$user_id,$current_user['id']);
+    $users[$key]['not_read'] = count_not_read_messages($db_connect, $user_id, $current_user['id']);
 }
 
-// Список всех сообщений в рамках одного активного чата
 $sql_messeges = "SELECT * FROM messages WHERE (sender = ? AND receiver = ?) OR (receiver = ? AND sender = ?) ORDER BY dt_add ASC";
-$messages = get_db_data($db_connect, $sql_messeges, [$active_chat, $current_user['id'], $active_chat, $current_user['id']]);
-// Данные пользователя с которым ведется переписка в рамках чата
+$messages = get_db_data($db_connect, $sql_messeges,
+    [$active_chat, $current_user['id'], $active_chat, $current_user['id']]);
+
 $sql_active_chat_user = "SELECT id, login, avatar FROM users WHERE id = ?";
-$active_chat_user = get_db_data($db_connect, $sql_active_chat_user, [$active_chat])[0];
 
-// Если отправлена форма с сообщением
+$active_chat_user = '';
+if (get_db_data($db_connect, $sql_active_chat_user, [$active_chat])) {
+    $active_chat_user = get_db_data($db_connect, $sql_active_chat_user, [$active_chat])[0];
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $new_message  = $_POST;
+    $current_url = $_SERVER['HTTP_REFERER'];
+    $new_message = $_POST;
 
-    // Очищаем сообщение от пробелом в начале, конце строки
     $new_message['message'] = trim($new_message['message'], ' ');
-    // Проверяем что поле не пустое
+
     $validation_rules = [
         'message' => ['required']
     ];
+
     $errors = validate($new_message ?? [], $validation_rules, $db_connect);
 
+    if (!count_lines_db_table($db_connect, 'id', 'users', 'id', $new_message['receiver'])) {
+        $errors['message'] = 'Пользователю нельзя отправить сообщение или его не существует';
+    };
 
-    // Если есть ошибки возвращаем их на исходную страницу
-    if (count($errors)) {
-        $_SESSION['message'] = $errors;
+    if ($new_message['receiver'] == $current_user['id']) {
+        $errors['message'] = 'Вы не можете отправить сообщение самому себе';
     }
-    // Если ошибок нет добавляем сообщение в бд и обновляем страницу с чатом
-    if (!count($errors)) {
-        $new_message['sender'] = $current_user['id'];
 
-        $sql = "INSERT INTO messages (dt_add, message, receiver, sender) VALUES (NOW(), ?,?,?)";
-        $stmt = db_get_prepare_stmt($db_connect, $sql, $new_message);
-        $res = mysqli_stmt_execute($stmt);
-        if ($res) {
-            header("Location: {$current_url}");
-        }
+    if (count($errors)) {
+        $content_data = [
+            'users' => $users,
+            'active_chat' => $active_chat,
+            'current_user' => $current_user,
+            'messages' => $messages,
+            'active_chat_user' => $active_chat_user,
+            'errors' => $errors
+        ];
+
+        print_messages_page($content_data, $current_user);
+        die();
+    }
+
+    $new_message['sender'] = $current_user['id'];
+
+    $sql = "INSERT INTO messages (dt_add, message, receiver, sender) VALUES (NOW(), ?,?,?)";
+    $stmt = db_get_prepare_stmt($db_connect, $sql, $new_message);
+    $res = mysqli_stmt_execute($stmt);
+    if ($res) {
+        header("Location: {$current_url}");
     }
 }
 
-
-$content = include_template('messages.php', [
+$content_data = [
     'users' => $users,
     'active_chat' => $active_chat,
     'current_user' => $current_user,
     'messages' => $messages,
     'active_chat_user' => $active_chat_user
-]);
+];
 
-$layout_content = include_template('layout.php', [
-    'content' => $content,
-    'title' => $title,
-    'current_user' => $current_user,
-    'active_page' => $active_page
-]);
+print_messages_page($content_data, $current_user);
 
-print($layout_content);
--- templates/messages.php
+++ templates/messages.php
@@ -1,17 +1,18 @@
 <?php
-$errors = [];
+/*$errors = [];
 if (isset($_SESSION['message'])) {
     $errors = $_SESSION['message'];
     unset($_SESSION['message']);
 }
-?>
+*/?>
 <main class="page__main page__main--messages">
     <h1 class="visually-hidden">Личные сообщения</h1>
     <section class="messages tabs">
         <h2 class="visually-hidden">Сообщения</h2>
         <div class="messages__contacts">
             <ul class="messages__contacts-list tabs__list">
-                <?php foreach ($users as $user) : ?>
+                <?php if (!isset($no_message) && isset($users)) : ?>
+                    <?php foreach ($users as $user) : ?>
                     <li class="messages__contacts-item">
                         <a class="messages__contacts-tab tabs__item <?=($active_chat == $user['id'] ? 'messages__contacts-tab--active tabs__item--active' : '')?>" href="messages.php?chat=<?=htmlspecialchars($user['id'])?>">
                             <div class="messages__avatar-wrapper">
@@ -24,6 +25,7 @@ if (isset($_SESSION['message'])) {
                                   <span class="messages__contact-name">
                                     <?=htmlspecialchars($user['login'])?>
                                   </span>
+                                <?php if ($user['message'] != '') :?>
                                 <div class="messages__preview">
                                     <p class="messages__preview-text">
                                         <?php if (htmlspecialchars($user['message']['sender']) == $current_user['id']) : ?>
@@ -38,41 +40,45 @@ if (isset($_SESSION['message'])) {
                                         <?= date('d.m', strtotime($user['message']['dt_add'])) ?>
                                     </time>
                                 </div>
+                                <?php endif; ?>
                             </div>
                         </a>
                     </li>
                 <?php endforeach;?>
+                <?php endif; ?>
             </ul>
         </div>
         <div class="messages__chat">
             <div class="messages__chat-wrapper">
                 <ul class="messages__list tabs__content tabs__content--active">
-                    <?php foreach ($messages as $message) : ?>
-                        <?php $user = ($message['sender'] == $current_user['id']) ? $current_user : $active_chat_user ?>
-                        <li class="messages__item <?=($message['sender'] == $current_user['id']) ? 'messages__item--my' : '' ?>">
-                            <div class="messages__info-wrapper">
-                                <div class="messages__item-avatar">
-                                    <a class="messages__author-link" href="profile.php?user=<?=htmlspecialchars($user['id'])?>">
-                                        <img class="messages__avatar" src="<?=$user['avatar']?>"
-                                             alt="Аватар пользователя">
-                                    </a>
+                    <?php if (!isset($no_message) && isset($messages)) : ?>
+                        <?php foreach ($messages as $message) : ?>
+                            <?php $user = ($message['sender'] == $current_user['id']) ? $current_user : $active_chat_user ?>
+                            <li class="messages__item <?=($message['sender'] == $current_user['id']) ? 'messages__item--my' : '' ?>">
+                                <div class="messages__info-wrapper">
+                                    <div class="messages__item-avatar">
+                                        <a class="messages__author-link" href="profile.php?user=<?=htmlspecialchars($user['id'])?>">
+                                            <img class="messages__avatar" src="<?=$user['avatar']?>"
+                                                 alt="Аватар пользователя">
+                                        </a>
+                                    </div>
+                                    <div class="messages__item-info">
+                                        <a class="messages__author" href="profile.php?user=<?=htmlspecialchars($user['id'])?>">
+                                            <?=htmlspecialchars($user['login'])?>
+                                        </a>
+                                        <time class="messages__time"
+                                              datetime="<?= $message['dt_add'] ?>"
+                                              title="<?= date('d.m.Y H:i', strtotime($message['dt_add'])) ?>">
+                                            <?= elapsed_time($message['dt_add']) ?>
+                                        </time>
+                                    </div>
                                 </div>
-                                <div class="messages__item-info">
-                                    <a class="messages__author" href="profile.php?user=<?=htmlspecialchars($user['id'])?>">
-                                        <?=htmlspecialchars($user['login'])?>
-                                    </a>
-                                    <time class="messages__time"
-                                          datetime="<?= $message['dt_add'] ?>"
-                                          title="<?= date('d.m.Y H:i', strtotime($message['dt_add'])) ?>">
-                                        <?= elapsed_time($message['dt_add']) ?>
-                                    </time>
-                                </div>
-                            </div>
-                            <p class="messages__text">
-                                <?=htmlspecialchars($message['message'])?>
-                            </p>
-                        </li>
-                    <?php endforeach; ?>
+                                <p class="messages__text">
+                                    <?=htmlspecialchars($message['message'])?>
+                                </p>
+                            </li>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
                 </ul>
             </div>
             <div class="comments">
@@ -82,14 +88,16 @@ if (isset($_SESSION['message'])) {
                             <img class="comments__picture" src="<?=$current_user['avatar']?>" alt="Аватар пользователя">
                         <?php endif; ?>
                     </div>
-                    <div class="form__input-section <?= (count($errors) && $errors['message']) ? 'form__input-section--error' : '' ?>">
+                    <div class="form__input-section <?= ($errors['message']) ? 'form__input-section--error' : '' ?>">
                         <textarea class="comments__textarea form__textarea form__input"
                           placeholder="Ваше сообщение" name="message"></textarea>
                         <label class="visually-hidden">Ваше сообщение</label>
+                        <?php if (!empty($errors)) : ?>
                         <button class="form__error-button button" type="button">!</button>
                         <div class="form__error-text">
                             <p class="form__error-desc"><?= $errors['message'] ?></p>
                         </div>
+                        <?php endif; ?>
                     </div>
                     <div class="form__input-section">
                         <input type="hidden" class="form__input" name="receiver" value="<?=htmlspecialchars($active_chat_user['id'])?>">
